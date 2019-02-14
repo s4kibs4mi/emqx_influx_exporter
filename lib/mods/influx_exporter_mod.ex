f8534f06a4d81307fb640929602c20012766d3dd
Sakib Sami <s4kibs4mi@gmail.com>
2019-02-14 17:59:22 +0600

    - Added : message acked

--- lib/mods/influx_exporter_mod.ex
+++ lib/mods/influx_exporter_mod.ex
@@ -12,6 +12,7 @@ defmodule InfluxExporterMod do
     hook_add(:"client.connected", &InfluxExporterMod.on_client_connected/4, [env])
     hook_add(:"message.publish", &InfluxExporterMod.on_message_publish/2, [env])
     hook_add(:"message.delivered", &InfluxExporterMod.on_message_delivered/3, [env])
+    hook_add(:"message.acked", &InfluxExporterMod.on_message_acked/3, [env])
     hook_add(:"client.disconnected", &InfluxExporterMod.on_client_disconnected/3, [env])
   end
 
@@ -19,6 +20,7 @@ defmodule InfluxExporterMod do
     hook_del(:"client.connected", &InfluxExporterMod.on_client_connected/4)
     hook_del(:"message.publish", &InfluxExporterMod.on_message_publish/2)
     hook_del(:"message.delivered", &InfluxExporterMod.on_message_delivered/3)
+    hook_del(:"message.acked", &InfluxExporterMod.on_message_acked/3)
     hook_del(:"client.disconnected", &InfluxExporterMod.on_client_disconnected/3)
   end
 
@@ -64,6 +66,24 @@ defmodule InfluxExporterMod do
     data = "#{data}message_delivered type=\"#{type}\"\n"
     data = "#{data}message_delivered qos=#{qos}i\n"
     data = "#{data}message_delivered id=\"#{id}\""
+
+    case InfluxExporterCLIHelper.write_event(data) do
+      :ok -> :logger.info("Write successful")
+      {:error, err} -> :logger.error(err)
+    end
+
+    {:ok, msg}
+  end
+
+  def on_message_acked(client, msg, _env) do
+    {type, id, qos, client_id, flags, user_info, topic, payload, time} = msg
+
+    data = ""
+    data = "#{data}message_acked client=\"#{client_id}\"\n"
+    data = "#{data}message_acked topic=\"#{topic}\"\n"
+    data = "#{data}message_acked type=\"#{type}\"\n"
+    data = "#{data}message_acked qos=#{qos}i\n"
+    data = "#{data}message_acked id=\"#{id}\""
 
     case InfluxExporterCLIHelper.write_event(data) do
       :ok -> :logger.info("Write successful")
